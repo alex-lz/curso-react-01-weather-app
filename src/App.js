1e3a005e61514cc52c0862a0705d674d2b7e7e3a
Alexander Starz <alexalm2495@gmail.com>
2020-03-13 18:57:03 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WeatherLocation from './componets/WeatherLocation'
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherLocation />
+    </div>
+  );
+}
+
+export default App;
